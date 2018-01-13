814d791bb254e463283a8841917069ce3c9369b4
ppdigger <shaoxuhui33@hotmail.com>
2018-01-13 13:23:20 +0800

    /

--- app/socket/index.js
+++ app/socket/index.js
@@ -1,10 +1,12 @@
 module.exports = (io) => {
     const liveUser = new Set();
-
+    
     io.on('connection', (socket) => {
+        const socketId = socket.id
         console.log(`一位用户上线`);
-        socket.on('login', function(data) {
-            console.log(data)
+        
+        socket.on('login', (userId) => {
+            console.log(userId)
         });
 
         socket.on("disconnect", () => {
--- config/redis.js
+++ config/redis.js
@@ -1,13 +1,13 @@
 var config = require('./config'),
 	redis = require('redis');
 
-// client = redis.createClient(config.redis)
-// client.on('ready', () => {
-// 	console.log('redis start at 172.0.0.1:6379')
-// })
+client = redis.createClient(config.redis)
+client.on('ready', () => {
+	console.log('redis start at 172.0.0.1:6379')
+})
 
-// client.on("error", (err) => {
-// 	console.log("Error " + err);
-// })
+client.on("error", (err) => {
+	console.log("Error " + err);
+})
 
-// module.exports = client
+module.exports = client
